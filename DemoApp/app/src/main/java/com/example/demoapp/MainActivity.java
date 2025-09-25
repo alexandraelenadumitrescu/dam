6a02c212f8d8953530211b9964a7099fa8095cd2
alexandraelenadumitrescu <134046146+alexandraelenadumitrescu@users.noreply.github.com>
2025-09-25 17:13:36 +0300

    lansarea unei activitati din interiorul altei activitati

--- DemoApp/app/src/main/java/com/example/demoapp/MainActivity.java
+++ DemoApp/app/src/main/java/com/example/demoapp/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.demoapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +10,12 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
 public class MainActivity extends AppCompatActivity {
+
+    private FloatingActionButton fabAdd;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +27,21 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+fabAdd=findViewById(R.id.main_fab_add);
+fabAdd.setOnClickListener(v->{
+    Intent intent=new Intent(getApplicationContext(),AddActivity.class);
+    startActivity(intent);
+});
     }
 }
+
+//fabAdd=findViewById(R.id.main_fab_add);
+//fabAdd.setOnClickListener(new View.OnClickListener() {
+//    @Override
+//    public void onClick(View v) {
+//        //v.getId();//=>returneaza R.id.main_fab_add de la ce buton s a declansat, daca implementam la nivelul clasei mainactivity view.onclicklistener o sg impl de clcik pt toate butoanele din interfata, nu se recomanda,mai ok inline sau anonim
+//    }
+//});
+//    }
+//}
